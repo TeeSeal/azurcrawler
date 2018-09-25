11f9b7cf64874eb08bff8b704b104b0f33fdbdc1
TeeSeal <teesealz@gmail.com>
2018-09-25 11:35:31 +0300

    Update shared functions for parsers

--- parsers/shared.py
+++ parsers/shared.py
@@ -1,16 +1,24 @@
-from os import makedirs
-from os.path import realpath, dirname, join, isdir
+from os import makedirs, listdir
+from os.path import realpath, dirname, join, isdir, abspath
 from bs4 import BeautifulSoup
 from json import dump
 
-ROOT = join(dirname(realpath(__file__)), '..')
+ROOT = abspath(join(dirname(realpath(__file__)), '..'))
 FIXTURES = join(ROOT, 'fixtures')
 DATA = join(ROOT, 'data')
 
 
 def read_fixture(dir_name, file_name):
-    fp = open(join(FIXTURES, dir_name, f'{file_name}.html'))
+    if not file_name.endswith('.html'):
+        file_name = f'{file_name}.html'
+
+    fp = open(join(FIXTURES, dir_name, file_name))
     return BeautifulSoup(fp, 'html.parser')
+
+
+def read_all_fixtures(dir_name):
+    dir_path = join(FIXTURES, dir_name)
+    return [read_fixture(dir_name, fixture) for fixture in listdir(dir_path)]
 
 
 def save_json(file_name, json):
