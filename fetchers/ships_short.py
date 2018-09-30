1039843e1c1df7c01d9bedba79e41323c0d5e7ea
Crawl <icrawltogo@gmail.com>
2018-09-30 14:14:50 +0200

    feat: add thumbnail fetching

--- fetchers/ships_short.py
+++ fetchers/ships_short.py
@@ -3,3 +3,6 @@ from shared import save_fixture
 
 result = get('https://azurlane.koumakan.jp/List_of_Ships')
 save_fixture('ships_short', 'list_of_ships', result.text)
+
+result = get('https://azurlane.koumakan.jp/List_of_Ships_by_Image')
+save_fixture('ships_short', 'list_of_ships_by_image', result.text)
